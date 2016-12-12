111e3bdc6603bb56fee8b85c5caab136db4b9700
Arthur PISSIER <apissier@e3r2p4.42.fr>
2016-12-12 15:18:22 +0100

    fillit.h

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: apissier <apissier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/08 16:56:09 by apissier          #+#    #+#             */
-/*   Updated: 2016/12/09 14:16:57 by apissier         ###   ########.fr       */
+/*   Updated: 2016/12/12 15:17:32 by apissier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,39 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 
+struct		s_pieces
+{
+	char			*all_codes[16*3];
+	unsigned short	all_pieces[16][4];
+
+	all_code = "3003040034034040403030204444000030034430004400044";
+	all_pieces = {	{16384, 57344, 0, 0}, \
+					{16384, 49152, 16384, 0}, \
+					{57344, 16384, 0, 0}, \
+					{32768, 49152, 32768, 0}, \
+					{32768, 49152, 16384, 0}, \
+					{49152, 24576, 0, 0}, \
+					{16384, 49152, 32768, 0}, \
+					{24576, 49152, 0, 0}, \
+					{32768, 32768, 32768, 32768}, \
+					{61440, 0, 0, 0}, \
+					{49152, 49152, 0, 0}, \
+					{49152, 32768, 32768, 0}, \
+					{16384, 16384, 49152, 0}, \
+					{57344, 8192, 0, 0}, \
+					{32768, 57344, 0, 0}, \
+					{49152, 16384, 16384, 0} };
+}
+
 char		**ft_read_file(char *str);
 int			ft_isvalid(char *t);
-void		*ft_realloc(void *ptr, size_t cur_size, size_t new_size);
+char		*ft_realloc(char *str, size_t size);
 void		ft_putstr(char const *s);
 size_t		ft_strlen(const char *s);
 void		*ft_memalloc(size_t size);
 char		*ft_code(char *str);
 void		*ft_memset(void *s, int c, size_t n);
+char		*ft_strnew(size_t size);
+char		*ft_strncpy(char *dest, const char *src, size_t n);
 
 #endif
